3fe0f06d1c474d925baa4d113b4c96f4ddf571af
Engraya <ahmadyakubuaya@gmail.com>
2026-05-23 21:45:13 +0100

    Fixed Ts Error

--- lib/types.ts
+++ lib/types.ts
@@ -3,4 +3,5 @@ import { links } from "./constants";
 /** Nav sections from `links`, plus hidden `#contact` when Contact component is mounted */
 export type SectionName =
   | (typeof links)[number]["name"]
-  | "Contact";
+  | "Contact"
+  | "Certifications";
